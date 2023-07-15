47290e9e9ce13c2b40c5ee4c2c6bb25af988e74e
Antoine Dricot <antdricot@gmail.com>
2023-07-15 23:04:13 +0200

    add game object with sprite and draw method

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 'use strict';
+
+import { GameObject } from './game_object.js';
 import {MyCanvas} from './my_canvas.js';
 import {ZZFX, zzfx} from './ZzFX.js'
 
@@ -7,6 +9,15 @@ export class Game {
         this.step = 0;
         this.previousTimestamp;
         this.myCanvas = new MyCanvas();
+
+        this.player = new GameObject(100, 200, 0, 90, 90) // FIXME
+    }
+
+    draw() {
+        this.myCanvas.clearRect();
+        this.myCanvas.fillBackground();
+        // TODO loop through all objects
+        this.player.draw(this.myCanvas.context, this.step)
     }
 
     run(timestamp) {
@@ -21,13 +32,13 @@ export class Game {
         this.step++;
         console.debug(' --- this.step = ' + this.step); // DEBUG
 
+        // TODO Menu etc. : initialize
         // Control
         // Move
+        this.player.x++; // FIXME
         // -- Collision?
-        // Draw (TODO method)
-        this.myCanvas.clearRect();
-        this.myCanvas.fillBackground();
-        
+        this.draw();
+
         if (this.step % 60 == 0) // FIXME remove (DEBUG)
             // TODO sound bank
             zzfx(...[,,9,,.21,.13,3,2.21,,,-226,.35,,.1,365,,,,.14,.02]); // Random 386
---
+++ js/game_object.js
@@ -0,0 +1,46 @@
+'use strict';
+
+export class GameObject {
+    constructor(x, y, z, width, height) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        // TODO dx, dy, dz
+        this.width = width;
+        this.height = height;
+
+        this.spriteSheet = new Image();
+        this.spriteSheet.src = "../sprites/daco.png";
+    }
+
+    // TODO class Drawing.drawRect
+    draw(context, step) {
+        // Draw box
+        context.beginPath();
+        context.fillStyle = null;
+        context.strokeStyle = 'green';
+        context.lineWidth = 1;
+        context.rect(
+            this.x, this.y, this.width, this.height);
+        // context.fill();
+        context.stroke();
+        context.closePath();
+
+        // Character
+
+        // TODO Move to own class e.g. Wolf
+        let hbWidth = 90; // hb = hit box
+        let hbHeight = 90;
+
+        // -- Draw sprite
+        let sx = 96 * (Math.floor(step / 10) % 4);
+        console.log(sx)
+        context.drawImage(
+            this.spriteSheet,
+            sx,
+            88*1, // -> state
+            90, 90,
+            this.x, this.y,
+            hbWidth, hbHeight);
+    }
+}
